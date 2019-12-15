472cab2ff822e23c82c429232e76fe989b2ff6e3
Paul Rafferty <raffraffraff@gmail.com>
2019-12-15 16:45:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # For now, I just want to be able to build this locally and test it.
-# I will eventually fine it a home on Docker Hub.
+# I will eventually find it a home on Docker Hub.
 
 docker build -t rippled:latest .
